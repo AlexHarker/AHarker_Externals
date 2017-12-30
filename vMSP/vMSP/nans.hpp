64277660a5b538fd0e0913a6bb1df5698db0bbed
Alex Harker <ajharker@gmail.com>
2017-12-30 16:39:06 +0000

    Functional NaN fixing

--- vMSP/vMSP/nans.hpp
+++ vMSP/vMSP/nans.hpp
@@ -6,11 +6,6 @@
 
 struct nan_fixer
 {
-    // Empty implementation for now...
-    
-    template <class T>
-    T operator()(const T a) { return a; }
-    
     float operator()(const float a)
     {
         uint32_t a_int = *reinterpret_cast<const uint32_t *>(&a);
@@ -21,6 +16,33 @@ struct nan_fixer
     {
         uint64_t a_int = *reinterpret_cast<const uint64_t *>(&a);
         return ((a_int & 0x7FF0000000000000ULL) == 0x7FF0000000000000ULL) && ((a_int & 0x000FFFFFFFFFFFFFULL) != 0) ? 0.0 : a;
+    }
+    
+    SIMDType<float, 1> operator()(const SIMDType<float, 1> a)       { return operator()(a.mVal); }
+    SIMDType<double, 1> operator()(const SIMDType<double, 1> a)     { return operator()(a.mVal); }
+    
+    template <int N>
+    SIMDType<float, N> operator()(const SIMDType<float, N> a)
+    {
+        const static uint32_t nan_mask_32 = 0x7F800000U;
+        const static uint32_t inf_mask_32 = 0x7FFFFFFFU;
+        
+        const SIMDType<float, N> v_nan_mask_32 = *reinterpret_cast<const float *>(&nan_mask_32);
+        const SIMDType<float, N> v_inf_mask_32 = *reinterpret_cast<const float *>(&inf_mask_32);
+        
+        return sel(a, SIMDType<float, N>(0.f), (v_nan_mask_32 == (a & v_nan_mask_32)) & (v_nan_mask_32 != (a & v_inf_mask_32)));
+    }
+    
+    template <int N>
+    SIMDType<double, N> operator()(const SIMDType<double, N> a)
+    {
+        const static uint64_t nan_mask_64 = 0x7FF0000000000000U;
+        const static uint64_t inf_mask_64 = 0x7FFFFFFFFFFFFFFFU;
+
+        const SIMDType<double, N> v_nan_mask_64 = *reinterpret_cast<const double *>(&nan_mask_64);
+        const SIMDType<double, N> v_inf_mask_64 = *reinterpret_cast<const double *>(&inf_mask_64);
+        
+        return sel(a, SIMDType<double, N>(0.0), (v_nan_mask_64 == (a & v_nan_mask_64)) & (v_nan_mask_64 != (a & v_inf_mask_64)));
     }
     
     template <class T>
@@ -36,4 +58,5 @@ struct nan_fixer
             *v_io =  operator()(*v_io);
     }
 };
+
 #endif /* NANS_H */
