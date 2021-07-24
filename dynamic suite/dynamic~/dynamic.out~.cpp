192386918d084e43c5e073fb03d5ed3f525ba63a
Alex Harker <ajharker@gmail.com>
2021-07-24 18:24:47 +0100

    Clean-up for the dynamic.out~ object

--- dynamic suite/dynamic~/dynamic.out~.cpp
+++ dynamic suite/dynamic~/dynamic.out~.cpp
@@ -4,9 +4,9 @@
  *
  *	dynamic.out~ acts like out~ but for patchers loaded inside a dynamicdsp~ object.
  *	
- *	Unlike out~ the user can change the signal outlet which the object refers to by sending an int to the object.
+ *	Unlike out~ you can change the signal outlet which the object refers to by sending an int to the object.
  *
- *  Copyright 2010 Alex Harker. All rights reserved.
+ *  Copyright 2010-21 Alex Harker. All rights reserved.
  *
  */
 
@@ -15,14 +15,16 @@
 #include <z_dsp.h>
 #include <ext_obex.h>
 
-#include <dynamic~.hpp>
 #include <SIMDSupport.hpp>
 
+#include <dynamic~.hpp>
+
+
+// Class and object structure
 
 t_class *this_class;
 
-
-typedef struct _dynamic_out
+struct t_dynamic_out
 {
     t_pxobject x_obj;
 	
@@ -31,37 +33,40 @@ typedef struct _dynamic_out
 	
 	t_atom_long outlet_num;
 	
-} t_dynamic_out;
+};
 
+// Function prototypes
 
-void dynamic_out_free(t_dynamic_out *x);
 void *dynamic_out_new(t_atom_long out_num);
+void dynamic_out_free(t_dynamic_out *x);
 void dynamic_out_assist(t_dynamic_out *x, void *b, long m, long a, char *s);
 
-void dynamic_out_dsp(t_dynamic_out *x, t_signal **sp, short *count);
-void dynamic_out_dsp64 (t_dynamic_out *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
-
 void dynamic_out_int(t_dynamic_out *x, t_atom_long intin);
+
 t_int *dynamic_out_perform_scalar(t_int *w);
 t_int *dynamic_out_perform(t_int *w);
 void dynamic_out_perform_scalar64 (t_dynamic_out *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam);
 void dynamic_out_perform64 (t_dynamic_out *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam);
 
+void dynamic_out_dsp(t_dynamic_out *x, t_signal **sp, short *count);
+void dynamic_out_dsp64 (t_dynamic_out *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
+
+// Main
 
 int C74_EXPORT main()
 {
 	this_class = class_new("dynamic.out~",
-						   (method) dynamic_out_new, 
-						   (method)dynamic_out_free, 
+						   (method) dynamic_out_new,
+						   (method) dynamic_out_free,
 						   sizeof(t_dynamic_out), 
 						   NULL, 
 						   A_DEFLONG, 
 						   0);
     
-	class_addmethod(this_class, (method)dynamic_out_dsp, "dsp", A_CANT, 0);
-	class_addmethod(this_class, (method)dynamic_out_dsp64, "dsp64", A_CANT, 0);
-    class_addmethod(this_class, (method)dynamic_out_assist, "assist", A_CANT, 0);
-	class_addmethod(this_class, (method)dynamic_out_int, "int", A_LONG, 0);
+	class_addmethod(this_class, (method) dynamic_out_dsp, "dsp", A_CANT, 0);
+	class_addmethod(this_class, (method) dynamic_out_dsp64, "dsp64", A_CANT, 0);
+    class_addmethod(this_class, (method) dynamic_out_assist, "assist", A_CANT, 0);
+	class_addmethod(this_class, (method) dynamic_out_int, "int", A_LONG, 0);
     
 	class_dspinit(this_class);
 	
@@ -70,10 +75,7 @@ int C74_EXPORT main()
 	return 0;
 }
 
-void dynamic_out_free(t_dynamic_out *x)
-{
-	dsp_free(&x->x_obj);
-}
+// New / Free / Assist
 
 void *dynamic_out_new(t_atom_long outlet_num)
 {
@@ -87,7 +89,12 @@ void *dynamic_out_new(t_atom_long outlet_num)
 		
     dsp_setup((t_pxobject *)x, 1);
     
-    return (x);
+    return x;
+}
+
+void dynamic_out_free(t_dynamic_out *x)
+{
+    dsp_free(&x->x_obj);
 }
 
 void dynamic_out_assist(t_dynamic_out *x, void *b, long m, long a, char *s)
@@ -95,146 +102,105 @@ void dynamic_out_assist(t_dynamic_out *x, void *b, long m, long a, char *s)
     sprintf(s,"(signal) Signal Output %ld of Patcher / (int) Outlet Number",(long)  x->outlet_num);
 }
 
-void dynamic_out_dsp(t_dynamic_out *x, t_signal **sp, short *count)
-{
-    const static int simd_width = SIMDLimits<float>::max_size;
-
-	if (((t_ptr_size) sp[0]->s_vec) % 16 || sp[0]->s_n < simd_width)
-		dsp_add(dynamic_out_perform_scalar, 3, sp[0]->s_vec, sp[0]->s_n, x);		// misaligned vector (not 16-byte aligned) or small vector sizes
-	else
-		dsp_add(dynamic_out_perform, 3, sp[0]->s_vec, sp[0]->s_n, x);				// aligned vector (should be the case from max 5.1.3 onwards
-}
-
-void dynamic_out_dsp64(t_dynamic_out *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
-{
-    const static int simd_width = SIMDLimits<double>::max_size;
-    
-	if (maxvectorsize >= simd_width)
-		object_method(dsp64, gensym("dsp_add64"), x, dynamic_out_perform64, 0, NULL);				// aligned vector 
-    else
-		object_method(dsp64, gensym("dsp_add64"), x, dynamic_out_perform_scalar64, 0, NULL);		// scalar routine
-}
+// Int Handler
 
 void dynamic_out_int (t_dynamic_out *x, t_atom_long intin)
 {
 	x->outlet_num = intin;
 }
 
-// Perform Routine for misaligned vectors or small vector sizes (done in scalar code)
+// Perform
+
+// Template perform routine
+
+template <typename T>
+void dynamic_out_perform(t_dynamic_out *x, T *in, long N)
+{
+    T ***out_handle = (T ***) x->out_handle;
+    T *io_pointer;
+    
+    long num_sig_outs = x->num_sig_outs;
+    t_atom_long outlet_num = x->outlet_num;
+    
+    if (!out_handle || !*out_handle)
+        return;
+
+    if (outlet_num >= 1 && outlet_num <= num_sig_outs)
+    {
+        if ((io_pointer = (*out_handle)[outlet_num - 1]))
+        {
+            for (long i = 0; i < N; i++, io_pointer++)
+                *io_pointer = *io_pointer + *in++;
+        }
+    }
+}
+
+// Scalar Perform Routine
 
 t_int *dynamic_out_perform_scalar(t_int *w)										
 {	
-    float *in1 = (float *)(w[1]);
+    float *in = (float *)(w[1]);
     long vec_size = w[2];
     t_dynamic_out *x = (t_dynamic_out *)(w[3]);
 	
-	float ***out_handle = (float ***) x->out_handle;
-    float *io_pointer;
-	
-	long outlet_num = x->outlet_num;
-	long num_sig_outs = x->num_sig_outs;
-	
-    if (!out_handle || !*out_handle)
-        return w + 4;
-
-    if (outlet_num > 0 && outlet_num <= num_sig_outs)
-    {
-        if ((io_pointer = (*out_handle)[outlet_num - 1]))
-		{
-			for (long i = 0; i < vec_size; i++)
-				*io_pointer++ += *in1++;
-		}
-	}
+    dynamic_out_perform(x, in, vec_size);
 	
     return w + 4;
 }
 
-// Aligned SIMD Perform Routine
+// SIMD Perform Routine
 
 t_int *dynamic_out_perform(t_int *w)												
 {
     using SIMD = SIMDType<float, SIMDLimits<float>::max_size>;
-    SIMD *in1 = (SIMD *)(w[1]);
+    SIMD *in = (SIMD *)(w[1]);
     long vec_size = w[2];
     t_dynamic_out *x = (t_dynamic_out *)(w[3]);
-	
-	SIMD ***out_handle = (SIMD ***) x->out_handle;
-    SIMD *io_pointer;
-	
-    const long num_vecs = vec_size / SIMDLimits<float>::max_size;
-	long num_sig_outs = x->num_sig_outs;
-	long outlet_num = x->outlet_num;
-	
-    if (!out_handle || !*out_handle)
-         return w + 4;
-    
-    if (outlet_num > 0 && outlet_num <= num_sig_outs)
-    {
-        if ((io_pointer = (*out_handle)[outlet_num - 1]))
-        {
-            for (long i = 0; i < num_vecs; i++)
-            {
-                *io_pointer = *io_pointer + *in1++;
-                io_pointer++;
-            }
-        }
-    }
+		
+    dynamic_out_perform(x, in, vec_size / SIMDLimits<float>::max_size);
 	
     return w + 4;
 }
 
-// Perform Routine for misaligned vectors or small vector sizes (done in scalar code) 64 Bit
+// Scalar Perform Routine 64 Bit
 
 void dynamic_out_perform_scalar64(t_dynamic_out *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam)
-{	
-    double *in1 = ins[0];	
-	double *io_pointer;
-	double ***out_handle = (double ***) x->out_handle;
-	
-	t_atom_long outlet_num = x->outlet_num;
-	long num_sig_outs = x->num_sig_outs;
-	
-    if (!out_handle || !*out_handle)
-        return;
-    
-    if (outlet_num > 0 && outlet_num <= num_sig_outs)
-    {
-        if ((io_pointer = (*out_handle)[outlet_num - 1]))
-        {
-			for (long i = 0; i < vec_size; i++)
-				*io_pointer++ += *in1++;
-				io_pointer++;
-		}
-	}
+{
+    dynamic_out_perform(x, ins[0], vec_size);
 }
 
-// Aligned SIMD Perform Routine 64 Bit
+// SIMD Perform Routine 64 Bit
 
 void dynamic_out_perform64(t_dynamic_out *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam)
 {
     using SIMD = SIMDType<double, SIMDLimits<double>::max_size>;
-
-    const long num_vecs = vec_size / SIMDLimits<double>::max_size;
-    SIMD *in1 = (SIMD *) ins[0];
-	SIMD *io_pointer;
-	SIMD ***out_handle = (SIMD ***) x->out_handle;
+    SIMD *in = (SIMD *) ins[0];
 	
-	long num_sig_outs = x->num_sig_outs;
-	t_atom_long outlet_num = x->outlet_num;
-	
-	if (!out_handle || !*out_handle)
-        return;
-    
-    if (outlet_num > 0 && outlet_num <= num_sig_outs)
-    {
-        if ((io_pointer = (*out_handle)[outlet_num - 1]))
-        {
-            for (long i = 0; i < num_vecs; i++)
-            {
-                *io_pointer = *io_pointer + *in1++;
-                io_pointer++;
-            }
-        }
-    }
+    dynamic_out_perform(x, in, vec_size /  SIMDLimits<double>::max_size);
 }
 
+// DSP
+
+void dynamic_out_dsp(t_dynamic_out *x, t_signal **sp, short *count)
+{
+    const static int simd_width = SIMDLimits<float>::max_size;
+    
+    // Use SIMD routines where possible
+
+    if (((t_ptr_size) sp[0]->s_vec) % 16 || sp[0]->s_n < simd_width)
+        dsp_add(dynamic_out_perform_scalar, 3, sp[0]->s_vec, sp[0]->s_n, x);
+    else
+        dsp_add(dynamic_out_perform, 3, sp[0]->s_vec, sp[0]->s_n, x);
+}
+
+void dynamic_out_dsp64(t_dynamic_out *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
+{
+    const static int simd_width = SIMDLimits<double>::max_size;
+    
+    // Use SIMD routines where possible
+    
+    if (maxvectorsize >= simd_width)
+        object_method(dsp64, gensym("dsp_add64"), x, dynamic_out_perform64, 0, NULL);
+    else
+        object_method(dsp64, gensym("dsp_add64"), x, dynamic_out_perform_scalar64, 0, NULL);
+}
