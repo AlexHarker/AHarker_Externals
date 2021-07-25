db515ee30621d7ac34f98485d94f76288a9075a0
Alex Harker <ajharker@gmail.com>
2021-07-25 23:10:11 +0100

    Remove items not needed from Max 6.1 onwards

--- sample accurate suite/voicedrive~/voicedrive~.cpp
+++ sample accurate suite/voicedrive~/voicedrive~.cpp
@@ -44,12 +44,7 @@ void *voicedrive_new(t_atom_long precision, t_atom_long interrupt_on);
 void voicedrive_free (t_voicedrive *x);
 void voicedrive_assist (t_voicedrive *x, void *b, long m, long a, char *s);
 
-t_int *voicedrive_perform (t_int *w);
-t_int *voicedrive_perform_hr (t_int *w);
-
 void voicedrive_perform64 (t_voicedrive *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam);
-
-void voicedrive_dsp (t_voicedrive *x, t_signal **sp, short *count);
 void voicedrive_dsp64(t_voicedrive *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
 
 // Main
@@ -66,7 +61,6 @@ int C74_EXPORT main()
 						   0);
 	
 	class_addmethod(this_class, (method) voicedrive_assist, "assist", A_CANT, 0);
-	class_addmethod(this_class, (method) voicedrive_dsp, "dsp", A_CANT, 0);
 	class_addmethod(this_class, (method) voicedrive_dsp64, "dsp64", A_CANT, 0);
 	
 	class_dspinit(this_class);
@@ -104,181 +98,7 @@ void voicedrive_free(t_voicedrive *x)
 	dsp_free(&x->x_obj);
 }
 
-// Perform
-
-// Standard Perform Routine
-
-t_int *voicedrive_perform (t_int *w)
-{	
-	// Set pointers
-	
-	float *in1 = (float *) w[1];
-	float *in2 = (float *) w[2];
-	float *in3 = (float *) w[3];
-	float *in4 = (float *) w[4];
-	float *out1 = (float *) w[5];
-	float *out2 = (float *) w[6];
-	float *out3 = (float *) w[7];
-	float *out4 = (float *) w[8];
-	long vec_size = w[9];
-    t_voicedrive *x = (t_voicedrive *) w[10];
-	
-	bool interrupt_on = x->interrupt_on;
-	
-	float trigger, busy;
-	
-	double pos, norm;
-	double length = x->length;
-	double drive = x->drive;
-	double length_val = x->length_val;
-	double out_offset = x->out_offset;
-	double sr_val = x->sr_val;
-	double msr_recip = 1.0 / sr_val;
-	
-	while (vec_size--) 
-	{			
-		// Check for new trigger conditions
-		
-		if (*in1 && *in2 > 0.f && *in3 < 1.f && (interrupt_on || drive >= length))
-		{
-			trigger = 1.f;
-			length = *in2 * sr_val;
-			length_val = 1.0 / length;
-			drive = *in3;
-			out_offset = *in4;
-			
-			if (drive < 0.0)
-				drive = 0.0;
-		}
-		else 
-			trigger = 0.f;
-		
-		// Calculate current drive position and related parameters
-		
-		drive += 1.0;
-		
-		if (drive >= length)
-		{
-			drive = length;
-			busy = 0.f;
-		}
-		else
-			busy = 1.f;
-		
-		norm = drive * length_val;
-		pos = drive * msr_recip + out_offset;
-		
-		// Advance input pointers
-		
-		in1++;
-		in2++;
-		in3++;
-		in4++;
-		
-		// Output
-		
-		*out1++ = (float) pos;
-		*out2++ = (float) norm;
-		*out3++ = trigger;
-		*out4++ = busy;
-	}
-	
-	x->drive = drive;
-	x->length = length;
-	x->length_val = length_val;
-	x->out_offset = out_offset;
-	
-	return w + 11;
-}
-
-// High-Resolution Perform Routine
-
-t_int *voicedrive_perform_hr (t_int *w)
-{	
-	// Set pointers
-	
-	float *in1 = (float *) w[1];
-	float *in2 = (float *) w[2];
-	float *in3 = (float *) w[3];
-	float *in4 = (float *) w[4];
-	float *out1 = (float *) w[5];
-	float *out2 = (float *) w[6];
-	float *out3 = (float *) w[7];
-	float *out4 = (float *) w[8];
-	float *out5 = (float *) w[9];
-	long vec_size = w[10];
-    t_voicedrive *x = (t_voicedrive *) w[11];
-	
-	bool interrupt_on = x->interrupt_on;
-	
-	float trigger, busy, fpos;
-	
-	double pos, norm;
-	double length = x->length;
-	double drive = x->drive;
-	double length_val = x->length_val;
-	double out_offset = x->out_offset;
-	double sr_val = x->sr_val;
-	double msr_recip = 1.0 / sr_val;
-	
-	while (vec_size--) 
-	{			
-		// Check for new trigger conditions
-
-		if (*in1 && *in2 > 0.f && *in3 < 1.f && (interrupt_on || drive >= length))
-		{
-			trigger = 1.f;
-			length = *in2 * sr_val;
-			length_val = 1.0 / length;
-			drive = *in3;
-			out_offset = *in4;
-			
-			if (drive < 0.0)
-				drive = 0.0;
-		}
-		else 
-			trigger = 0.f;
-		
-		// Calculate current drive position and related parameters
-
-		drive += 1.0;
-		
-		if (drive >= length)
-		{
-			drive = length;
-			busy = 0.f;
-		}
-		else
-			busy = 1.f;
-		
-		norm = drive * length_val;
-		pos = drive * msr_recip + out_offset;
-		
-		// Advance input pointers
-		
-		in1++;
-		in2++;
-		in3++;
-		in4++;
-		
-		// Output
-		
-		*out1++ = fpos = (float) pos;
-		*out2++ = (float) pos - fpos;
-		*out3++ = (float) norm;
-		*out4++ = trigger;
-		*out5++ = busy;
-	}
-	
-	x->length = length;
-	x->length_val = length_val;
-	x->drive = drive;
-	x->out_offset = out_offset;
-	
-	return w + 12;
-}
-
-// 64 bit Perform Routine
+// Perform Routine
 
 void voicedrive_perform64(t_voicedrive *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam)
 {	
@@ -358,8 +178,8 @@ void voicedrive_perform64(t_voicedrive *x, t_object *dsp64, double **ins, long n
 	// Zero the hr output if in precision mode
 	
 	if (hr_out)
-		for (i = 0; i < vec_size--; i++) 
-			hr_out[i] = 0.;
+		for (i = 0; i < vec_size; i++)
+			hr_out[i] = 0.0;
 	
 	x->length = length;
 	x->length_val = length_val;
@@ -368,16 +188,6 @@ void voicedrive_perform64(t_voicedrive *x, t_object *dsp64, double **ins, long n
 }
 
 // DSP
-
-void voicedrive_dsp(t_voicedrive *x, t_signal **sp, short *count)
-{
-    x->sr_val = sp[0]->s_sr / 1000.0;
-    
-    if (x->precision)
-        dsp_add(voicedrive_perform_hr, 11, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec, sp[6]->s_vec, sp[7]->s_vec, sp[8]->s_vec, sp[0]->s_n, x);
-    else
-        dsp_add(voicedrive_perform, 10, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec, sp[6]->s_vec, sp[7]->s_vec, sp[0]->s_n, x);
-}
 
 void voicedrive_dsp64(t_voicedrive *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
 {			
