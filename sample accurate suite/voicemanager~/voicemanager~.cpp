35a53eb566d1323aefa64047e33fa72457f09397
Alex Harker <ajharker@gmail.com>
2021-07-26 09:03:50 +0100

    REmove items not needed for Max 6.1 onwards

--- sample accurate suite/voicemanager~/voicemanager~.cpp
+++ sample accurate suite/voicemanager~/voicemanager~.cpp
@@ -49,10 +49,7 @@ void voicemanager_assist(t_voicemanager *x, void *b, long m, long a, char *s);
 void voicemanager_reset(t_voicemanager *x);
 void voicemanager_active(t_voicemanager *x, t_symbol *msg, long argc, t_atom *argv);
 
-t_int *voicemanager_perform(t_int *w);
 void voicemanager_perform64(t_voicemanager *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam);
-
-void voicemanager_dsp(t_voicemanager *x, t_signal **sp, short *count);
 void voicemanager_dsp64(t_voicemanager *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
 
 // Main
@@ -70,7 +67,6 @@ int C74_EXPORT main()
 	class_addmethod(this_class, (method) voicemanager_reset, "reset", 0);
 	class_addmethod(this_class, (method) voicemanager_reset, "active", A_GIMME,0);
 	class_addmethod(this_class, (method) voicemanager_assist, "assist", A_CANT, 0);
-	class_addmethod(this_class, (method) voicemanager_dsp, "dsp", A_CANT, 0);
 	class_addmethod(this_class, (method) voicemanager_dsp64, "dsp64", A_CANT, 0);
 	
 	class_dspinit(this_class);
@@ -130,100 +126,6 @@ void voicemanager_active(t_voicemanager *x, t_symbol *msg, long argc, t_atom *ar
 }
 
 // Perform
-
-t_int *voicemanager_perform(t_int *w)
-{	
-	// Set pointers
-	
-	float *in1 = (float *) w[1];
-	float *in2 = (float *) w[2];
-	float *in3 = (float *) w[3];
-	float *in4 = (float *) w[4];
-	float *out1 = (float *) w[5];
-	float *out2 = (float *) w[6];
-	float *out3 = (float *) w[7];
-	long vec_size = w[8];
-    t_voicemanager *x = (t_voicemanager *) w[9];
-	
-	long max_voices = x->max_voices;
-	long active_voices = x->active_voices;
-	long active_connected = x->active_connected;
-	long voice_number;
-	long i;
-	
-	double *free_times = x->free_times;
-		
-	double length;
-	double subsample_offset;
-	double current_time = x->current_time;
-	double sr_val = x->sr_val;
-	
-	// Reset the data for all voices
-	
-	if (x->reset)
-	{
-		for (i = 0; i < max_voices; i++)
-			free_times[i] = current_time;
-		
-		current_time = 0.0;
-		x->reset = false;
-	}
-	
-	// Sample loop
-	
-	while (vec_size--) 
-	{
-		// Reset variables
-		
-		voice_number = 0;
-		length = 0.0;
-		subsample_offset = 0.0;
-		
-		active_voices = active_connected ? (long) *in4 : active_voices;
-		active_voices  = active_voices > max_voices ? max_voices : active_voices;
-		
-		if (*in1 && (*in3  < 1.0))
-		{
-			// Search for a free and active voice
-			
-			for (voice_number = 1; free_times[voice_number - 1] > current_time && voice_number <= active_voices; voice_number++);
-			
-			if (voice_number <= active_voices)
-			{
-				// Get data for trigger
-				
-				length = *in2;
-				subsample_offset = *in3;
-				
-				if (subsample_offset < 0.0)
-					subsample_offset = 0.0;
-				
-				free_times[voice_number - 1] = current_time + (sr_val * length) - subsample_offset;
-			}
-			else 
-				voice_number = 0;
-		}
-		
-		// Advance input pointers 
-		
-		in1++;
-		in2++;
-		in3++;
-		in4++;
-		
-		// Output parameters
-		
-		*out1++ = (float) voice_number;
-		*out2++ = (float) length;
-		*out3++ = (float) subsample_offset;
-		
-		current_time += 1.0;
-	}
-	
-	x->current_time = current_time;
-	
-	return w + 10;
-}
 
 void voicemanager_perform64(t_voicemanager *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam)
 {	
@@ -316,14 +218,6 @@ void voicemanager_perform64(t_voicemanager *x, t_object *dsp64, double **ins, lo
 }
 
 // DSP
-
-void voicemanager_dsp(t_voicemanager *x, t_signal **sp, short *count)
-{
-    x->sr_val = sp[0]->s_sr / 1000.0;
-    x->active_connected = count[3];
-    
-    dsp_add(voicemanager_perform, 9, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec, sp[6]->s_vec, sp[0]->s_n, x);
-}
 
 void voicemanager_dsp64(t_voicemanager *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
 {			
