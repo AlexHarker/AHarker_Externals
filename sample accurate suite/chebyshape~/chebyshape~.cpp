96e5774ded05219dd4fd5de1ee313ca48e08f9e5
Alex Harker <ajharker@gmail.com>
2021-09-04 12:09:45 +0100

    Tabs -> spaces

--- sample accurate suite/chebyshape~/chebyshape~.cpp
+++ sample accurate suite/chebyshape~/chebyshape~.cpp
@@ -94,9 +94,9 @@ void *chebyshape_new(t_atom_long num_coeff, t_atom_long offset)
     x->offset = std::max(0L, std::min(static_cast<long>(offset), x->num_sig_ins));
     
     if (!x->offset)
-		dsp_setup((t_pxobject *)x, static_cast<long>(num_coeff + 1));
-	else
-		dsp_setup((t_pxobject *)x, 2);
+        dsp_setup((t_pxobject *)x, static_cast<long>(num_coeff + 1));
+    else
+        dsp_setup((t_pxobject *)x, 2);
     
     outlet_new((t_object *)x, "signal");
     
