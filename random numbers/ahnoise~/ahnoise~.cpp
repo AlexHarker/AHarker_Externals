c644c6ac29033b58fc989473519d42ff6d6ade6f
Alex Harker <ajharker@gmail.com>
2021-11-13 13:24:27 +0000

    Minor formatting issues

--- random numbers/ahnoise~/ahnoise~.cpp
+++ random numbers/ahnoise~/ahnoise~.cpp
@@ -24,9 +24,9 @@ t_class *this_class;
 
 struct t_ahnoise
 {
-	t_pxobject a_obj;
-	
-	random_generator<> gen;
+    t_pxobject a_obj;
+    
+    random_generator<> gen;
 };
 
 // Function Prototypes
@@ -44,23 +44,23 @@ void ahnoise_dsp64(t_ahnoise *x, t_object *dsp64, short *count, double samplerat
 // Main
 
 int C74_EXPORT main()
-{	
-	this_class = class_new("ahnoise~",
-						   (method)ahnoise_new,
-						   (method)ahnoise_free,
-						   sizeof(t_ahnoise), 
-						   NULL, 
-						   0);
-	
-	class_addmethod(this_class, (method)ahnoise_dsp, "dsp", A_CANT, 0);
-	class_addmethod(this_class, (method)ahnoise_dsp64, "dsp64", A_CANT, 0);
-	class_addmethod(this_class, (method)ahnoise_assist, "assist", A_CANT, 0);
-	
-	class_dspinit(this_class);
-	
-	class_register(CLASS_BOX, this_class);
-	
-	return 0;
+{
+    this_class = class_new("ahnoise~",
+                           (method)ahnoise_new,
+                           (method)ahnoise_free,
+                           sizeof(t_ahnoise),
+                           nullptr,
+                           0);
+    
+    class_addmethod(this_class, (method)ahnoise_dsp, "dsp", A_CANT, 0);
+    class_addmethod(this_class, (method)ahnoise_dsp64, "dsp64", A_CANT, 0);
+    class_addmethod(this_class, (method)ahnoise_assist, "assist", A_CANT, 0);
+    
+    class_dspinit(this_class);
+    
+    class_register(CLASS_BOX, this_class);
+    
+    return 0;
 }
 
 // New / Free
@@ -70,16 +70,16 @@ void *ahnoise_new()
     t_ahnoise *x = (t_ahnoise *)object_alloc(this_class);
 
     dsp_setup((t_pxobject *)x, 0);
-	outlet_new((t_object *)x, "signal");
+    outlet_new((t_object *)x, "signal");
 
     new(&x->gen) random_generator<>();
-	
-	return (x);
+    
+    return x;
 }
 
 void ahnoise_free(t_ahnoise *x)
 {
-	dsp_free(&x->a_obj);
+    dsp_free(&x->a_obj);
     x->gen.~random_generator<>();
 }
 
@@ -97,8 +97,8 @@ void perform_core(T *out, long vec_size, random_generator<> *gen)
 t_int *ahnoise_perform(t_int *w)
 {
     perform_core(reinterpret_cast<float *>(w[1]), static_cast<long>(w[2]), reinterpret_cast<random_generator<> *>(w[3]));
-		
-	return w + 4;
+        
+    return w + 4;
 }
 
 void ahnoise_perform64(t_ahnoise *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vec_size, long flags, void *userparam)
@@ -109,25 +109,25 @@ void ahnoise_perform64(t_ahnoise *x, t_object *dsp64, double **ins, long numins,
 // DSP
 
 void ahnoise_dsp(t_ahnoise *x, t_signal **sp, short *count)
-{				
-	dsp_add(ahnoise_perform, 3, sp[0]->s_vec, sp[0]->s_n, &x->gen);
+{
+    dsp_add(ahnoise_perform, 3, sp[0]->s_vec, sp[0]->s_n, &x->gen);
 }
 
 void ahnoise_dsp64(t_ahnoise *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
-{				
-	object_method(dsp64, gensym("dsp_add64"), x, ahnoise_perform64, 0, NULL);
+{
+    object_method(dsp64, gensym("dsp_add64"), x, ahnoise_perform64, 0, nullptr);
 }
 
 // Assist
 
 void ahnoise_assist(t_ahnoise *x, void *b, long m, long a, char *s)
 {
-	if (m == ASSIST_OUTLET) 
-	{
-		sprintf(s,"(signal) Noise");
-	}
-    else 
-	{
-		sprintf(s,"(signal) Dummy");
+    if (m == ASSIST_OUTLET)
+    {
+        sprintf(s,"(signal) Noise");
+    }
+    else
+    {
+        sprintf(s,"(signal) Dummy");
     }
 }
